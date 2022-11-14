7636a5f6ea26d7d39bf366879591174e653eb6df
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2022-11-14 11:46:09 +0400

    Add files via upload

---
+++ 11.11.2022/11.11.2022/Program.cs
@@ -0,0 +1,69 @@
+﻿//#region task 2
+//Console.WriteLine("birinci ededi daxil edin: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("ikinci ededi daxil edin: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("araliq: ");
+
+//for (int i = num1+1; i <num2 ; i++)
+//{
+//    Console.WriteLine(i);
+//}
+//#endregion
+//#region task 3
+//Console.WriteLine("Reqemi daxil edin");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int defaultnum = 70;
+//if (num > 70)
+//{
+//    Console.WriteLine((num -= defaultnum) * 3);
+//}
+//else
+//{
+//    Console.WriteLine(defaultnum -= num);
+//}
+
+
+//#endregion
+//#region task 4
+//Console.WriteLine("X ededini daxil et:");
+//int numX = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Y ededini daxil et:");
+//int numY = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Z ededini daxil et:");
+//int numZ = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Boyuk eded:");
+
+//if (numX > numY && numX > numZ) ;
+//{
+//    Console.WriteLine(numX);
+//}
+//else if (numY > numX && numY > numZ)
+//{
+//    Console.WriteLine(numY);
+//}
+//else
+//{
+//    Console.WriteLine(numZ);
+//}
+//#endregion
+//#region
+//Console.WriteLine("N ededini daxil edin:");
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("M ededini daxil edin:");
+//int m = Convert.ToInt32(Console.ReadLine());
+//int count = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+    
+//}
+//Console.WriteLine("Tek reqemlerin sayi: " + count);
+
+
+//#endregion
